4383e4aa19d87520e8933bc017e43a8da986bdc0
dheeraj kumar <dheerajtocsi@gmail.com>
2026-03-31 23:02:19 +0530

    fix: comprehensive one-shot database type synchronization

--- src/main/resources/db/changelog/V007__fix_boolean_types.sql
+++ src/main/resources/db/changelog/V007__fix_boolean_types.sql
@@ -1,10 +1,17 @@
 --liquibase formatted sql
 --changeset enterprise:V007 labels:fix
---comment: Convert SMALLINT flags to BOOLEAN for PostgreSQL compatibility
+--comment: Comprehensive type repair for PostgreSQL compatibility (Booleans, Integers, BigInts)
 
+-- 1. Fix User Table
 ALTER TABLE WALLET_USERS 
     ALTER COLUMN IS_ACTIVE TYPE BOOLEAN USING IS_ACTIVE::boolean,
-    ALTER COLUMN IS_EMAIL_VERIFIED TYPE BOOLEAN USING IS_EMAIL_VERIFIED::boolean;
+    ALTER COLUMN IS_EMAIL_VERIFIED TYPE BOOLEAN USING IS_EMAIL_VERIFIED::boolean,
+    ALTER COLUMN FAILED_LOGIN_ATTEMPTS TYPE INTEGER USING FAILED_LOGIN_ATTEMPTS::integer;
 
+-- 2. Fix Wallet Table
+ALTER TABLE WALLETS
+    ALTER COLUMN VERSION TYPE BIGINT USING VERSION::bigint;
+
+-- 3. Fix Refresh Tokens
 ALTER TABLE REFRESH_TOKENS 
     ALTER COLUMN IS_REVOKED TYPE BOOLEAN USING IS_REVOKED::boolean;
